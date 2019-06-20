85de7b9df6090c6155b7245d0a42e3b010b5d251
Yevhenii Hurynets <yhurynets@alteryx.com>
2019-06-21 00:19:02 +0300

    fix to class

--- src/Form.js
+++ src/Form.js
@@ -1,21 +1,34 @@
 import React from 'react';
 
-const Form = (props) => {
-  const { onSubmit, item = {} } = props;
-  const [state, setState] = React.useState(item);
-  React.useEffect(() => {
-    setState(item);
-  }, [item]);
+class Form extends React.Component{
 
-  const { name, age } = state;
+  static getDerivedStateFromProps (props, state) {
+    if (props.item.id !== state.id) {
+      return { ...props.item }
+    }
+    return null
+  }
 
-  return (
-    <form onSubmit={e => e.preventDefault() || onSubmit(state)}>
-    <input type="text" value={name} onChange={e => setState({ ...state, name: e.target.value })}/>
-    <input type="text" value={age} onChange={e => setState({ ...state, age: e.target.value })}/>
-    <input type="submit"/>
-  </form>
-  )
+  state = {
+    name: '',
+    age: '',
+    id: ''
+  };
+
+  render() {
+    const { onSubmit } = this.props;
+    const { name, age } = this.state;
+
+    return (
+      <form onSubmit={e => e.preventDefault() || onSubmit(this.state)}>
+        <input type="text" value={name}
+               onChange={e => this.setState({...this.state, name: e.target.value})}/>
+        <input type="text" value={age}
+               onChange={e => this.setState({...this.state, age: e.target.value})}/>
+        <input type="submit"/>
+      </form>
+    )
+}
 };
 
 export default Form;
